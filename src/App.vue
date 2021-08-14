b74c51cb10c4398dd0ca43990a5c296e520f2fba
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-08-14 12:09:20 -0400

    inicialiando uma tela de login

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Login/>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+  body{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 16px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <div class="container border">
+            <div class="row h-100">
+                <div class="col-5" id="message">rodrigo</div>
+                <div class="col-7">silva</div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .container{
+        height: 600px;
+        margin-top: 100px;
+    }
+
+    #message{
+        background: #58af9c;
+    }
+</style>
